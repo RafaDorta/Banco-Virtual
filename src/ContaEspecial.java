438cf01b8dec984615a589641aa0fcdcc680cc86
RafaDorta <88380983+RafaDorta@users.noreply.github.com>
2021-10-06 10:33:07 -0300

    Classes Criadas

---
+++ src/ContaEspecial.java
@@ -0,0 +1,9 @@
+
+public class ContaEspecial extends Contas{
+	public ContaEspecial()
+	{
+		setTipo(3);
+		setRendimento(0);
+	}
+
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,13 @@
+
+public class ContaPoupanca extends Contas{
+
+	public ContaPoupanca()
+	{
+		setLimite(0);
+		
+		setTipo(2);
+		
+	}
+	
+
+}
